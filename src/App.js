bba6849cabd88f44dbfccafc9bd1d1aba2eb10fc
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-04-26 17:23:06 -0400

    finished connecting Login post call , now moving onto private routs, having issues with axios getting recipes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 
 import Home from "./routes/home/Home";
 import Login from "./routes/login/Login";
 import Register from "./routes/register/Register";
+import Recipes from "../src/routes/recipes/Recipes";
 
 function App() {
-  return (
-    <Switch>
-      {/**<PrivateRoute to="/account/:id" /> */}
-      {/**<PrivateRoute to="/recipes/add" /> */}
-      {/**<PrivateRoute to="/recipes/:id/edit" /> */}
-      <Route path="/recipes/:id" />
-      <Route path="/recipes" />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register}/>
-      <Route exact path="/" component={Home}/>
-    </Switch>
-  );
+	return (
+		<Switch>
+			{/**<PrivateRoute to="/account/:id" /> */}
+			{/* *<PrivateRoute to="/recipes/add" /> */}
+			{/**<PrivateRoute to="/recipes/:id/edit" /> */}
+			<Route path="/recipes/:id" />
+			<Route path="/recipes" component={Recipes} />
+			<Route path="/login" history={useHistory()} component={Login} />
+			<Route path="/register" history={useHistory()} component={Register} />
+			<Route exact path="/" component={Home} />
+		</Switch>
+	);
 }
 
 export default App;
